8c4134206a4f414619f32b484fbe7b49528ae58c
EC2 Default User <ec2-user@ip-172-31-32-72.ap-northeast-1.compute.internal>
2020-07-28 07:57:48 +0000

    [Update]add guest user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,13 @@ class HomeController < ApplicationController
   def index
   end
   
+  def new_guest
+    user = User.find_or_create_by(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "ゲストユーザー"
+    end
+    sign_in user
+    redirect_to pairings_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   root 'home#index'
+  post '/home/guest_sign_in', to: 'home#new_guest'
   devise_for :users
   resources :pairings, only: [:new, :create, :index, :show, :destroy, :edit, :update] do
     resource :favorites, only: [:create, :destroy]
